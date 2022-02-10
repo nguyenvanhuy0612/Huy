6a0808f020e349da22d9036edbf8c04ce4860b52
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2022-02-10 12:33:52 +0700

    add

--- JavaHuyExample/src/test/java/com/training/LoginVBSCA.java
+++ JavaHuyExample/src/test/java/com/training/LoginVBSCA.java
@@ -4,15 +4,31 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
 public class LoginVBSCA {
+
+
+    @Test
+    public void DateTest() {
+        final SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+        Calendar cal = Calendar.getInstance();
+    }
+
 
     @Test
     public void EnterTest() {
         System.out.println("Init driver");
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
         System.out.println("Enter to url");
         driver.get("http://vbsca.ca/login/");
@@ -41,6 +57,13 @@ public class LoginVBSCA {
 
         Common.waitSec(5);
 
+        WebDriverWait wait = new WebDriverWait(driver, 60);
+        //wait.until(ExpectedConditions.numberOfElementsToBe())
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Proactive Outreach Dashboard']")));
+
+        if (new WebDriverWait(driver, 5).until(ExpectedConditions.alertIsPresent()) != null ){
+            driver.switchTo().alert().accept();
+        }
         System.out.println("Close driver");
         driver.close();
 
---
+++ PythonExample/Test/test.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
+
+
+def test():
+    print("Hello")
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(30)
+    driver.find_element(By.XPATH, "//*[contains(text(), 'Username')]")
+
+
+test()
