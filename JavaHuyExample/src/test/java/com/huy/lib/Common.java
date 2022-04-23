258840a4f9f0248236c2eb89cadbf5c08ae25abe
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2022-04-23 09:34:07 +0700

    add

--- JavaHuyExample/src/test/java/com/huy/lib/Common.java
+++ JavaHuyExample/src/test/java/com/huy/lib/Common.java
@@ -1,15 +1,12 @@
 package com.huy.lib;
 
-//import io.cucumber.java.af.En;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public class Common {
 
