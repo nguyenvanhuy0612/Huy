7b3f4de55a11f60a9831a61477ff219bb29c023d
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2022-04-23 09:33:28 +0700

    add

--- JavaHuyExample/src/test/java/com/huy/lib/Common.java
+++ JavaHuyExample/src/test/java/com/huy/lib/Common.java
@@ -1,6 +1,6 @@
 package com.huy.lib;
 
-import io.cucumber.java.af.En;
+//import io.cucumber.java.af.En;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
