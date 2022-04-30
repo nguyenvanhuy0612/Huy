89e36b1874684b09ea9cb53844de59652f02b7e2
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2022-04-30 23:23:03 +0700

    update

--- JavaHuyExample/src/test/java/com/test/Test7_XML.java
+++ JavaHuyExample/src/test/java/com/test/Test7_XML.java
@@ -1,5 +1,11 @@
 package com.test;
 
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.MutableTriple;
+import org.apache.commons.lang3.tuple.Pair;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import org.w3c.dom.*;
 import org.xml.sax.InputSource;
@@ -13,6 +19,7 @@ import java.util.*;
 import java.util.function.Consumer;
 
 public class Test7_XML {
+    WebDriver driver;
 
     @Test
     public void testXml() throws Exception {
@@ -50,14 +57,37 @@ public class Test7_XML {
         String midBaseLoc = "//div[@id='destinationTree']";
         String rightBaseLoc = "//div[@id='inner-right-component']";
 
-        String nodeProperties = "//div[@id='propertiesBody']";
+        String nodePropertiesLoc = "//div[@id='propertiesBody']";
+        String nodePropertiesRowLoc = "//div[@id='propertiesBody']//div[@class='tableRow']";
+
+        for (int i = 0; i < driver.findElements(By.xpath(nodePropertiesRowLoc)).size(); i++) {
+
+        }
+
 
         //div[@id='destinationTree']//a[contains(@id, 'AvayaPIMContactStrategy')]
         String contactStrategyLoc = midBaseLoc + "//a[contains(@id, 'AvayaPIMContactStrategy')]";
 
         String menuLoc = "//ul[contains(@class, 'jstree-default-contextmenu')]";
         String addNodeLoc = menuLoc + "//a[contains(text(), 'Add Node')]";
+    }
 
+
+    public List<MutableTriple<String, String, WebElement>> getPropertyElement() {
+        //key, type, element
+        List<MutableTriple<String, String, WebElement>> propertyElementList = new ArrayList<>();
+
+        String nodePropertiesLoc = "//div[@id='propertiesBody']";
+        String nodePropertiesRowLoc = "//div[@id='propertiesBody']//div[@class='tableRow']";
+
+        for (int i = 0; i < driver.findElements(By.xpath(nodePropertiesRowLoc)).size(); i++) {
+            String currColumnLoc = nodePropertiesRowLoc + "[" + i + "]/div[contains(@class, 'tableColumn')]";
+            String key = driver.findElement(By.xpath(currColumnLoc + "[1]")).getAttribute("data-property-section");
+            WebElement element = driver.findElement(By.xpath(currColumnLoc + "[2]//*[contains(@data-property, '@')]"));
+            String type = element.getTagName();
+            propertyElementList.add(MutableTriple.of(key, type, element));
+        }
+        return propertyElementList;
     }
 
     public void nodeProcess(Node n) {
@@ -71,14 +101,14 @@ public class Test7_XML {
         for (Node node : nodeList) {
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element eElement = (Element) node;
-                List<Map.Entry<String, String>> attrList = new ArrayList<>();
+                List<MutablePair<String, String>> attrList = new ArrayList<>();
                 String nodeName = eElement.getTagName();
                 // drag and drop for node name
                 System.out.println("nodeName: " + nodeName);
                 NamedNodeMap attributes = eElement.getAttributes();
                 if (attributes.getLength() == 0) {
                     System.out.println("nodeName -> TEXT: " + node.getTextContent());
-                    attrList.add(new AbstractMap.SimpleEntry<>(eElement.getTagName(), node.getTextContent()));
+                    attrList.add(MutablePair.of(eElement.getTagName(), node.getTextContent()));
 
                 }
                 for (int j = 0; j < attributes.getLength(); j++) {
@@ -87,7 +117,7 @@ public class Test7_XML {
                     String attrValue = attr.getValue();
                     System.out.println("attrName: " + attrName);
                     System.out.println("attrValue: " + attrValue);
-                    attrList.add(new AbstractMap.SimpleEntry<>(attr.getName(), attr.getValue()));
+                    attrList.add(MutablePair.of(attr.getName(), attr.getValue()));
                 }
                 // process attribute for node name
 
