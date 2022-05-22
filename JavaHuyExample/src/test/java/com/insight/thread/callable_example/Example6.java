159020cf9aab089cf43f8e017f7a4df1155c63ff
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2022-05-22 16:50:28 +0700

    Add

--- JavaHuyExample/src/test/java/com/insight/thread/callable_example/Example6.java
+++ JavaHuyExample/src/test/java/com/insight/thread/callable_example/Example6.java
@@ -1,11 +1,9 @@
 package com.insight.thread.callable_example;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class Example6 {
 
@@ -13,9 +11,23 @@ public class Example6 {
     public void test() {
         ExecutorService executor = Executors.newFixedThreadPool(10);
         Callable<String> c = () -> {
+            System.out.println("Start c");
+            Thread.sleep(6000);
             System.out.println(1 / 0);
             return null;
         };
         Future<String> future = executor.submit(c);
+        try {
+            System.out.println("future get start");
+            future.get();
+            System.out.println("future get finish");
+        } catch (Exception e) {
+        }
+        System.out.println(1 / 0);
+        try {
+            System.out.println(1 / 0);
+        } catch (Exception e) {
+            throw e;
+        }
     }
 }
