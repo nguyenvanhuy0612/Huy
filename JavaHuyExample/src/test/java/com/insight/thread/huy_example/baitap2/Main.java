0ac99d43a7c762bd43fdb0dd5903534ec1d626ed
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2022-06-19 21:02:38 +0700

    Add

--- JavaHuyExample/src/test/java/com/insight/thread/huy_example/baitap2/Main.java
+++ JavaHuyExample/src/test/java/com/insight/thread/huy_example/baitap2/Main.java
@@ -2,13 +2,73 @@ package com.insight.thread.huy_example.baitap2;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class Main {
+
     public static void main(String[] args) {
+        System.out.println("START");
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        List<Callable<String>> callableList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            final int _i = i;
+            //callableList.add(() -> "Callable " + _i);
+            callableList.add(() -> {
+                Thread.sleep(5000);
+                String name = "Callable " + _i + " " + Thread.currentThread().getName();
+                System.out.println(name);
+                return name;
+            });
+        }
+        System.out.println();
+
+        List<Future<String>> result = new ArrayList<>();
+        try {
+            System.out.println("invokeAll start");
+            result = executorService.invokeAll(callableList);
+            System.out.println("invokeAll finish");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+
+        result.forEach(r -> {
+            try {
+                System.out.println(r.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
+
+        executorService.shutdown();
+        System.out.println("END");
+    }
+
+    public static void main4(String[] args) {
+        System.out.println("START");
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        List<Callable<String>> callableList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            final int _i = i;
+            callableList.add(() -> "Callable " + _i);
+        }
+
+        String result = "";
+        try {
+            result = executorService.invokeAny(callableList);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("result: " + result);
+
+        executorService.shutdown();
+        System.out.println("END");
+    }
+
+    public static void main3(String[] args) {
         System.out.println("START");
         ExecutorService executorService = Executors.newFixedThreadPool(5);
         List<Future<String>> futures = new ArrayList<>();
