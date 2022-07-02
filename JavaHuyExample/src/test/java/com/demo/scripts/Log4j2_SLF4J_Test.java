af6586df1ac34c2ee2de322fd2e6bd0d5c148ae7
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2022-07-02 17:17:36 +0700

    Update

---
+++ JavaHuyExample/src/test/java/com/demo/scripts/Log4j2_SLF4J_Test.java
@@ -0,0 +1,15 @@
+package com.demo.scripts;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Log4j2_SLF4J_Test {
+    private static final Logger logger = LoggerFactory.getLogger(Log4j2_SLF4J_Test.class);
+
+    public static void main(final String[] args)
+    {
+        logger.debug("Debug Message Logged !!!");
+        logger.info("Info Message Logged !!!");
+        logger.error("Error Message Logged !!!", new NullPointerException("NullError"));
+    }
+}
