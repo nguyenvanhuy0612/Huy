a76fcb2941101dfdd94892695ad9a8d55096f324
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2022-07-02 17:58:33 +0700

    Update

--- JavaHuyExample/src/test/java/com/test/Env.java
+++ JavaHuyExample/src/test/java/com/test/Env.java
@@ -1,16 +1,44 @@
 package com.test;
 
+import com.practice.bdd_testng_demo.lib.EnvSetup;
 import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Env {
     public static WebDriver driver;
     public static WebDriver driver2;
 
-    public static void sleep(long sec){
+    public static void sleep(long sec) {
         try {
             Thread.sleep(sec * 1000);
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             System.out.println("InterruptedException");
         }
+    }
+
+    public static void main(String[] args) {
+        ClassLoader classLoader = EnvSetup.class.getClassLoader();
+        System.out.println(classLoader);
+        // get a file from the resource folder, root of classpath
+        System.out.println(classLoader.getResourceAsStream("user.json"));
+
+        File file = new File(".");
+        ClassLoader classLoader2 = Thread.currentThread().getContextClassLoader();
+
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+
+        System.out.println(Thread.currentThread().getStackTrace()[1].getClassName());
+
+        String filePath = System.getProperty("user.dir") + "/" + Thread.currentThread().getStackTrace()[1].getClassName().replace(".", "/");
+
+        System.out.println(filePath);
+
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        System.out.println("Current absolute path is: " + s);
+
     }
 }
