86aa0598838d08c6b9906dc0b9eee4fe823906e0
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2022-07-07 00:23:41 +0700

    UPDATE

--- JavaHuyExample/src/test/java/com/insight/common_func_collection/ExcelUtility.java
+++ JavaHuyExample/src/test/java/com/insight/common_func_collection/ExcelUtility.java
@@ -21,55 +21,73 @@ public class ExcelUtility {
             for (int i = 0; i < wb.getNumberOfSheets(); i++) {
                 // sheet
                 List<HashMap<String, String>> curSheetData = new ArrayList<>();
-                List<String> headerList = new ArrayList<>();
+                List<String> headerRowList = new ArrayList<>();
 
                 Sheet sheet = wb.getSheetAt(i);
-                short topRow = sheet.getTopRow();
-                int lastRow = sheet.getLastRowNum();
 
-                for (int j = topRow; j < lastRow; j++) {
-                    Row row = sheet.getRow(j);
-                    List<String> rowList = new ArrayList<>();
-                    row.forEach(cell -> {
-                        CellType cellType = cell.getCellType();
-                        switch (cellType) {
-                            case _NONE:
-                                rowList.add("NONE");
-                                break;
-                            case BLANK:
-                                rowList.add("BLANK");
-                                break;
-                            case ERROR:
-                                rowList.add("ERROR");
-                                break;
-                            case STRING:
-                                rowList.add(cell.getStringCellValue());
-                                break;
-                            case BOOLEAN:
-                                rowList.add(String.valueOf(cell.getBooleanCellValue()));
-                                break;
-                            case FORMULA:
-                                rowList.add("FORMULA");
-                                break;
-                            case NUMERIC:
-                                rowList.add(String.valueOf(cell.getNumericCellValue()));
-                                break;
-                            default:
-                                rowList.add("");
-                        }
-                    });
-                    if (j == topRow) {
-                        headerList.addAll(rowList);
-                    } else {
-                        HashMap<String, String> rowMap = new HashMap<>();
-                        for (int k = 0; k < headerList.size(); k++) {
-                            rowMap.put(headerList.get(k), rowList.get(k));
-                        }
-                        curSheetData.add(rowMap);
-                    }
-
+                Row headerRow = sheet.getRow(sheet.getTopRow());
+                for (Cell hCell : headerRow) {
+                    if (hCell.toString().isEmpty())
+                        break;
+                    headerRowList.add(hCell.toString());
                 }
-                System.out.println();
+
+                for (int j = sheet.getTopRow(); j < sheet.getLastRowNum(); j++) {
+                    Row curRow = sheet.getRow(j);
+                    if (j == sheet.getTopRow() || curRow.getCell(curRow.getFirstCellNum(), Row.MissingCellPolicy.CREATE_NULL_AS_BLANK).toString().isEmpty()) {
+                        continue;
+                    }
+                    HashMap<String, String> curRowMap = new HashMap<>();
+                    for (int k = headerRow.getFirstCellNum(); k < headerRowList.size(); k++) {
+                        curRowMap.put(headerRowList.get(k), curRow.getCell(k, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK).toString());
+                    }
+                    curSheetData.add(curRowMap);
+                }
+                results.put(sheet.getSheetName(), curSheetData);
+                //System.out.println(results);
+//                for (int j = topRow; j < lastRow; j++) {
+//                    Row row = sheet.getRow(j);
+//                    List<String> rowList = new ArrayList<>();
+//                    row.forEach(cell -> {
+//                        CellType cellType = cell.getCellType();
+//                        switch (cellType) {
+//                            case _NONE:
+//                                rowList.add("NONE");
+//                                break;
+//                            case BLANK:
+//                                rowList.add("BLANK");
+//                                break;
+//                            case ERROR:
+//                                rowList.add("ERROR");
+//                                break;
+//                            case STRING:
+//                                rowList.add(cell.getStringCellValue());
+//                                break;
+//                            case BOOLEAN:
+//                                rowList.add(String.valueOf(cell.getBooleanCellValue()));
+//                                break;
+//                            case FORMULA:
+//                                rowList.add("FORMULA");
+//                                break;
+//                            case NUMERIC:
+//                                rowList.add(String.valueOf(cell.getNumericCellValue()));
+//                                break;
+//                            default:
+//                                rowList.add("");
+//                        }
+//                    });
+//                    if (j == topRow) {
+//                        headerList.addAll(rowList);
+//                    } else {
+//                        HashMap<String, String> rowMap = new HashMap<>();
+//                        for (int k = 0; k < headerList.size(); k++) {
+//                            rowMap.put(headerList.get(k), rowList.get(k));
+//                        }
+//                        curSheetData.add(rowMap);
+//                    }
+//
+//                }
+//                System.out.println();
 //                for (Row row : sheet) {
 //                    // row
 //                    HashMap<String, String> curRowData = new HashMap<>();
