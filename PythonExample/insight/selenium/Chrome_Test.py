5108eb3b40aa28ad2c3fbac36f0d0fb33a328070
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2022-07-09 19:23:41 +0700

    Add

--- PythonExample/insight/selenium/Chrome_Test.py
+++ PythonExample/insight/selenium/Chrome_Test.py
@@ -1,10 +1,22 @@
 import time
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-driver.get("https://google.com")
-time.sleep(2)
-driver.quit()
+def test():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.implicitly_wait(30)
+
+    driver.get("https://google.com")
+    time.sleep(2)
+    driver.find_element(By.NAME, "q").send_keys("TEST SELENIUM", Keys.RETURN)
+
+    time.sleep(5)
+    driver.quit()
+
+
+if __name__ == "__main__":
+    test()
--- PythonExample/insight/selenium/Edge_Test.py
+++ PythonExample/insight/selenium/Edge_Test.py
@@ -4,7 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+service = EdgeService(EdgeChromiumDriverManager().install())
+
+driver = webdriver.Edge(service=service)
 driver.get("https://google.com")
 time.sleep(2)
 driver.quit()
