0c3bceb9ed66cfa4a9defd0907a21c78463d926d
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2022-07-10 20:04:38 +0700

    Add

--- PythonExample/insight/programiz_python/python_operators/operator.py
+++ PythonExample/insight/programiz_python/python_operators/operator.py
@@ -50,3 +50,52 @@ print('x and y is', x and y)
 print('x or y is', x or y)
 
 print('not x is', not x)
+
+# Bitwise operators
+x = 10
+y = 4
+# x | y = 14
+print(x | y)
+
+# Assignment operators
+x += 5
+
+# Special operators
+# Identity operators: is, is not
+x = False
+print(x is True)
+print(x is False)
+print(x is not False)
+# Example 4: Identity operators in Python
+x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+x3 = [1,2,3]
+y3 = [1,2,3]
+
+# Output: False
+print(x1 is not y1)
+
+# Output: True
+print(x2 is y2)
+
+# Output: False
+print(x3 is y3)
+
+# Membership operators
+# Example #5: Membership operators in Python
+x = 'Hello world'
+y = {1:'a',2:'b'}
+
+# Output: True
+print('H' in x)
+
+# Output: True
+print('hello' not in x)
+
+# Output: True
+print(1 in y)
+
+# Output: False
+print('a' in y)
