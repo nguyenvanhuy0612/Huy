e668832ffe79ccfa2d3cf8716f2e4343e8fb1791
nguyenvanhuy0612 <nguyenvanhuy0612@gmail.com>
2022-07-21 19:16:53 +0700

    Update

---
+++ JavaHuyExample/src/test/java/com/test/PrintList.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PrintList {
+    public static void main(String[] args) {
+        List<String> name = Arrays.asList("huy", "huy1");
+        System.out.println(name);
+    }
+}
